606f8732d9c24c2631d8df2addadfd50c9fa0fd3
RainPPR <PPR2125773894@163.com>
2026-03-31 11:48:20 +0800

    Release: v1.0.1

--- properdocs/commands/install_deps.py
+++ properdocs/commands/install_deps.py
@@ -10,8 +10,8 @@ log = logging.getLogger(__name__)
 @retry(
     # 1 initial attempt + 3 retries = 4 total attempts
     stop=stop_after_attempt(4),
-    # 1st retry waits 1s, 2nd waits 10s, 3rd waits 30s
-    wait=wait_chain(wait_fixed(1), wait_fixed(10), wait_fixed(30))
+    # 1st retry waits 1s, 2nd waits 30s, 3rd waits 30s
+    wait=wait_chain(wait_fixed(1), wait_fixed(30), wait_fixed(30))
 )
 def _download_pandoc_with_retry() -> None:
     """Download and install the pandoc executable with automatic retries."""
